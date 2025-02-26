3ab9f9e5075ae94ff038065ef1c2021a8062f91f
unknown <tadikondasrivardhini9@gmail.com>
2025-02-26 10:50:52 +0530

    .

--- index.js
+++ index.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const { resolve } = require('path');
 
+
+const MenuItem = require('./MenuItem');
 const app = express();
 const port = 3010;
 
+app.use(express.json())
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+app.post('/menu',async(req,res)=>{
+  try {
+    const menuItem=new MenuItem(req.body);
+    await menuItem.save
+    res.status(201).send({message: 'Menu item created',menuItem});
+  } catch (error) {
+    res.status(400).send({ error: error.message})
+  }
+});
+app.get('/menu',async(req,res)=>{
+  try {
+    const menuItem=await MenuItem.find();
+    res.status(200).send(menuItem);
+  } catch (error) {
+    res.status(500).send({error: error.message})
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
